b0c9cb00920bd497031a45b052a27684f7505cf8
Pedro Díaz <pdiazstam@gmail.com>
2014-01-25 19:37:42 -0500

    commit 4

---
+++ AddDeveloper/app/views/index.blade.php
@@ -0,0 +1,50 @@
+<!doctype html>
+<html lang="es">
+<head>
+	<meta charset="UTF-8" />
+	<title>Add Developers</title>
+	<meta name="viewport" content="initial-scale=1, maximum-scale=1">
+	<link rel="stylesheet" href="css/normalize.css">
+	<script type="text/javascript" scr="js/modernizr.min.js"></script>
+	<script type="text/javascript" scr="js/jquery.min.js"></script>
+</head>
+<body>
+	<nav>
+		<ul>
+			<li>
+				<div><a href="#">Nosotros</a></div>
+			</li>
+			<li>
+				<div><a href="#">Servicios</a></div>
+			</li>
+			<li>
+				<div><a href="#">Proyectos</a></div>
+			</li>
+			<li>
+				<div><a href="#">Contactenos</a></div>
+			</li>
+			<li>
+				<div><a href="#">Add Developers</a><a href="#"><div class="menu"></div></a></div>
+			</li>
+		</ul>
+	</nav>
+	<header>
+		<h1>Add Developers</h1>
+		<h4>Soluciones software a tu medida</h4>
+	</header>
+	<section>
+		
+	</section>
+	<footer>
+		<div class="ubicacion">
+			<div class="location">L</div> Av. Siempre viva 742 - Springfield
+		</div>
+		<div class="red_social">
+			<div class="facebook">F</div>
+			<div class="twitter">T</div>
+			<div class="linkedin">I</div>
+			<div class="git">git</div>
+		</div>
+	</footer>
+</body>
+</html>
